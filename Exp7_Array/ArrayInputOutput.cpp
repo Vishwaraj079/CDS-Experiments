d7271163ba22de9fe12496a0deb8984e50d3ac13
Vishwaraj <vishwaraj.murumkar.btech2022@sitpune.edu.in>
2025-10-31 03:35:41 +0530

    ArrayInputOutput.cpp

---
+++ Exp7_Array/ArrayInputOutput.cpp
@@ -0,0 +1,21 @@
+// Experiment 7 - Arrays
+// Program 1: Input and Output of array elements
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "You entered: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
