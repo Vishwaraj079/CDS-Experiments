a9c154ee8a70e04b5b85f641fdc877fbd9cf22ce
Vishwaraj <vishwaraj.murumkar.btech2022@sitpune.edu.in>
2025-10-31 03:36:22 +0530

    ReverseArray.cpp

---
+++ Exp7_Array/ReverseArray.cpp
@@ -0,0 +1,21 @@
+// Experiment 7 - Arrays
+// Program 2: Reverse an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Reversed array: ";
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+
+    return 0;
+}
