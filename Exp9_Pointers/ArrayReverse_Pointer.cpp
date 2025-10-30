70700a99f23e29442121da800a3c129fbeb83ae2
Vishwaraj <vishwaraj.murumkar.btech2022@sitpune.edu.in>
2025-10-31 03:51:27 +0530

    ArrayReverse_Pointer.cpp

---
+++ Exp9_Pointers/ArrayReverse_Pointer.cpp
@@ -0,0 +1,32 @@
+// Experiment 9 - Pointers
+// Program 3: Reverse array using pointers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int *start = arr;
+    int *end = arr + n - 1;
+
+    while (start < end) {
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
