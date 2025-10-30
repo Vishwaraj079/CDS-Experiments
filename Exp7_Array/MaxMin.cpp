a8234b559c1833f12f25979f29961dede0536b88
Vishwaraj <vishwaraj.murumkar.btech2022@sitpune.edu.in>
2025-10-31 03:39:31 +0530

    MaxMin.cpp

---
+++ Exp7_Array/MaxMin.cpp
@@ -0,0 +1,26 @@
+// Experiment 7 - Arrays
+// Program 5: Find maximum and minimum element in an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
+    }
+
+    cout << "Maximum = " << max << endl;
+    cout << "Minimum = " << min << endl;
+
+    return 0;
+}
