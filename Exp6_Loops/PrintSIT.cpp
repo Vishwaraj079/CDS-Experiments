6b34d04feb8261e3e5adc8935324852bd0f9d471
Vishwaraj <vishwaraj.murumkar.btech2022@sitpune.edu.in>
2025-10-31 03:29:46 +0530

    PrintSIT.cpp

---
+++ Exp6_Loops/PrintSIT.cpp
@@ -0,0 +1,11 @@
+// Experiment 6 - Loops
+// Program 2: Print "SIT" 5 times using for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << "SIT" << endl;
+    }
+    return 0;
+}
